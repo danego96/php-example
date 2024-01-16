bf96cafb6f09c6579a05cd0e8b8958fe35630535
Danila Egorov <danilaegorov@MacBook-Air-Danila.local>
2024-01-16 18:53:54 +0400

    first validation

--- app/controllers/post-create.php
+++ app/controllers/post-create.php
@@ -5,10 +5,30 @@
  */
 
  if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    $fillable = ['title', 'excerpt', 'content'];
+    $data = load($fillable);
+
+    //validation
+
+    $errors = [];
+    if(empty(trim($data['title']))){
+        $errors['title'] = 'Title is required';
+    }
+    if(empty(trim($data['excerpt']))){
+        $errors['excerpt'] = 'Excerpt is required';
+    }
+    if(empty(trim($data['content']))){
+        $errors['content'] = 'Content is required';
+    }
+
+if(empty($errors)){
     $db->query("INSERT INTO posts (title, excerpt, content) VALUES (?, ?, ?)", [
         $_POST['title'], $_POST['excerpt'], $_POST['content']
     ]);
  };
+}
+
 
 $title = "My Blog:: New Post"; 
 
--- app/views/post-create.tpl.php
+++ app/views/post-create.tpl.php
@@ -7,15 +7,30 @@
                 <form action="" method="post">
                     <div class="mb-3">
                         <label for="title" class="form-label">Post Title</label>
-                        <input name="title" type="text" class="form-control" id="title" placeholder="Post title">
+                        <input name="title" type="text" class="form-control" id="title" placeholder="Post title" required>
+                        <?php if(isset($errors['title'])): ?>
+                        <div class="invalid-feedback d-block">
+                        <?= $errors['title'] ?>
+                        </div>
+                        <?php endif; ?>
                     </div>
                     <div class="mb-3">
                         <label for="excerpt" class="form-label">Excerpt</label>
                         <textarea name="excerpt" class="form-control" id="excerpt" rows="3" placeholder="Post excerpt"></textarea>
+                        <?php if(isset($errors['excerpt'])): ?>
+                        <div class="invalid-feedback d-block">
+                        <?= $errors['excerpt'] ?>
+                        </div>
+                        <?php endif; ?>
                     </div>
                     <div class="mb-3">
                         <label for="content" class="form-label">Content</label>
                         <textarea name="content" class="form-control" id="content" rows="5" placeholder="Post content"></textarea>
+                        <?php if(isset($errors['content'])): ?>
+                        <div class="invalid-feedback d-block">
+                        <?= $errors['content'] ?>
+                        </div>
+                        <?php endif; ?>
                     </div>
                     <div class="mb-3">
                       <button type="submit" class="btn btn-primary">Create</button>
